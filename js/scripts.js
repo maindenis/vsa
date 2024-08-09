d0d3a0e519fb5d8eaa071dfd4da0bdea448a1545
maindenis <165322876+maindenis@users.noreply.github.com>
2024-08-09 09:39:03 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -13,14 +13,25 @@ function getRespParams() {
     }
 }
 
+// function getLineParams() {
+//     $("#line > div").css({
+//         "height" : "auto"
+//     });
+//     topCoord = $(".vsa_promo_slider .vsa_promo_slide:eq(0) .vsa_promo_slide_contant").offset().top;
+//     $("#line > div").css({
+//         "height" : topCoord + "px"
+//     });
+// }
+
 var w = window,
 d = document,
 e = d.documentElement,
 g = d.getElementsByTagName('body')[0],
 bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
-
+var topCoord;
 $(window).resize(function() {
     getRespParams();
+    // getLineParams();
     bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
 });
 
@@ -31,17 +42,18 @@ $(document).scroll(function() {
 $(document).ready(function() {
     getAnimate();
     getRespParams();
+    // getLineParams();
     // -----------
     if($('.owl-carousel-background').length > 0) {
         $('.owl-carousel-background').owlCarousel({
-            loop:true, //Зацикливаем слайдер
-            margin:0, //Отступ от элемента справа в 50px
-            nav:false, //Отключение навигации
-            autoplay:true, //Автозапуск слайдера
-            smartSpeed:1000, //Время движения слайда
-            autoplayTimeout:5000, //Время смены слайда
+            loop:true,
+            margin:0,
+            nav:false,
+            autoplay:true,
+            smartSpeed:1000,
+            autoplayTimeout:5000,
             lazyLoad: true,
-            lazyLoadEager: 2, //preload 3 items
+            lazyLoadEager: 2,
             animateIn: "owl-fadeIn",
             animateOut: "owl-fadeOut",
             video: true,
@@ -79,7 +91,17 @@ $(document).ready(function() {
         });
  }
 
-    if($('.vsa_promo_slider').length > 0) {
+    if($('.vsa_promo_slider').length > 0) {    
+    $('.vsa_promo_slider').on('initialized.owl.carousel', event => {
+        // getLineParams();
+        $("#line > div").css({
+            "height" : "auto"
+        });
+        topCoord = $(".vsa_promo_slider .vsa_promo_slide:eq(0) .vsa_promo_slide_contant").offset().top;
+        $("#line > div").css({
+            "height" : topCoord + "px"
+        });
+    });
      $('.vsa_promo_slider').owlCarousel({
         loop:true,
         margin:0,
@@ -100,6 +122,37 @@ $(document).ready(function() {
         touchDrag: false,
         singleItem: true
     });
+    $('.vsa_promo_slider').on('changed.owl.carousel', event => {
+        // getLineParams();
+        $("#line > div").css({
+            "height" : "auto"
+        });
+        topCoord = $(".vsa_promo_slider .owl-item.active .vsa_promo_slide_contant").offset().top;
+        $("#line > div").css({
+            "height" : topCoord + "px"
+        });
+    });
+    $('.vsa_promo_slider').on('resize.owl.carousel', event => {
+        // getLineParams();
+        $("#line > div").css({
+            "height" : "auto"
+        });
+        topCoord = $(".vsa_promo_slider .owl-item.active .vsa_promo_slide_contant").offset().top;
+        $("#line > div").css({
+            "height" : topCoord + "px"
+        });
+    });
+    $('.vsa_promo_slider').on('resized.owl.carousel', event => {
+        // getLineParams();
+        $("#line > div").css({
+            "height" : "auto"
+        });
+        topCoord = $(".vsa_promo_slider .owl-item.active .vsa_promo_slide_contant").offset().top;
+        $("#line > div").css({
+            "height" : topCoord + "px"
+        });
+    });
+
     }
 
     if($('.vsa_items_slider').length > 0) {
@@ -115,15 +168,29 @@ $(document).ready(function() {
         video: false,
         dots: false,
         autoWidth:true,
+        items:1,
         responsiveClass:false,
         responsive:{
         0:{
             margin:20,
             dots: true,
+            rtl: true,
+            autoWidth:false,
+            items:1,
+        },
+        450:{
+            margin:20,
+            dots: true,
+            rtl: true,
+            autoWidth:true,
+            items:1,
         },
         900:{
             margin:30,
             dots: false,
+            rtl: false,
+            autoWidth:true,
+            items:1,
         }
     }
     });
@@ -142,7 +209,21 @@ $(document).ready(function() {
         video: false,
         dots: false,
         autoWidth:true,
-        responsiveClass:false
+        responsiveClass:true,
+        responsive:{
+            0:{ 
+                rewind: true,
+                slideTransition: 'linear',
+                autoplaySpeed: 11000,
+                smartSpeed: 11000,      
+            },
+            900:{
+                smartSpeed:1000,
+                autoplayTimeout:2500,
+                slideTransition: false,
+                rewind: false,
+            }
+        }
     });
     }
 
@@ -160,7 +241,21 @@ $(document).ready(function() {
         video: false,
         dots: false,
         autoWidth:true,
-        responsiveClass:false
+        responsiveClass:true,
+        responsive:{
+            0:{ 
+                rewind: true,
+                slideTransition: 'linear',
+                autoplaySpeed: 11000,
+                smartSpeed: 11000,      
+            },
+            900:{
+                smartSpeed:1000,
+                autoplayTimeout:2500,
+                slideTransition: false,
+                rewind: false,
+            }
+        }
     });
     }
 
@@ -200,5 +295,10 @@ $(document).ready(function() {
         $(".respNavBox").fadeOut(300);
         $(".resp_btn").removeClass("active");
     });
+
+    // ------------
+
+    var image = document.getElementsByClassName('avs_article_2_bg');
+    new simpleParallax(image);
 
 });
